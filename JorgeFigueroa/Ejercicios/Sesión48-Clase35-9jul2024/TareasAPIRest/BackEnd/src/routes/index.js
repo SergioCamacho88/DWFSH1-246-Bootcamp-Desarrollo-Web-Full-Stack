dcd4dfb1b89cd06ce145c2623721b0f0c2ccd161
JFFB <jorgef.figueroa@gmail.com>
2024-07-11 17:49:45 -0500

    Add FrontEnd->APIRest->BD
    Se agrega el ejercicio de FrontEnd->APIRest->BD del ejercicio de YouTube de Fazt

---
+++ JorgeFigueroa/Ejercicios/Sesión48-Clase35-9jul2024/TareasAPIRest/BackEnd/src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = Router();
+
+router.get("/test", (req, res) => {
+  const data = {
+    name: "Fazt",
+    website: "faztweb.com",
+  };
+  res.json(data);
+});
+
+module.exports = router;
